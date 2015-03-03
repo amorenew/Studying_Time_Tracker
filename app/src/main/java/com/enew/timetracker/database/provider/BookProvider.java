f9138dfa129cd4d9ce3b0d2941f78db95bf18ed9
amorenew <amorenew@gmail.com>
2015-03-03 14:02:20 +0200

    more work in content provider

--- app/src/main/java/com/enew/timetracker/database/provider/BookProvider.java
+++ app/src/main/java/com/enew/timetracker/database/provider/BookProvider.java
@@ -3,18 +3,27 @@ package com.enew.timetracker.database.provider;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.enew.timetracker.database.handler.CategoryHandler;
 
 /**
  * Created by amorenew on 2/27/2015.
  */
 public class BookProvider extends ContentProvider {
-    public static final String AUTHORITY = "content://com.enew.timetracker.provider";
+    static final String AUTHORITY = "content://com.enew.timetracker.provider";
     public static final Uri CONTENT_URI = Uri.parse(AUTHORITY);
-
+    static final String SINGLE_RECORD_MIME_TYPE = "vnd.android.cursor.item/vnd.enew.timetracker.time";
+    static final String MULTIPLE_RECORD_MIME_TYPE = "vnd.android.cursor.dir/vnd.enew.timetracker.time";
+    SQLiteDatabase db;
+    CategoryHandler categoryHandler;
     @Override
     public boolean onCreate() {
-        return false;
+
+        return true;
     }
 
     @Override
@@ -24,7 +33,13 @@ public class BookProvider extends ContentProvider {
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        String ret = getContext().getContentResolver().getType(Settings.System.CONTENT_URI);
+        Log.d("content provider", "getType returning: " + ret);
+        if (uri.getLastPathSegment() == null) {
+            return SINGLE_RECORD_MIME_TYPE;
+        } else {
+            return MULTIPLE_RECORD_MIME_TYPE;
+        }
     }
 
     @Override
